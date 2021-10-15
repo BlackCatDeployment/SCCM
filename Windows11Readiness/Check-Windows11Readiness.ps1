283040381d06940f5800ae19cb13525989f55941
Black Cat Deployment <valentef.pro@gmail.com>
2021-10-15 11:41:07 +0200

    Update Check-Windows11Readiness.ps1

--- Windows11Readiness/Check-Windows11Readiness.ps1
+++ Windows11Readiness/Check-Windows11Readiness.ps1
@@ -5,8 +5,7 @@
     Check if a device is Windows 11 readiness.
     This script can be used with SCCM or other tools for getting the registry key value created.
     .OUTPUTS
-    Write a registry key "HKLM:\SOFTWARE\BlackCat" value "Win11Ready" with True or False data
-    Write a registry key "HKLM:\SOFTWARE\BlackCat" value "Win11Reason" with blocable reason
+    Write a registry key "HKLM:\SOFTWARE\BlackCat" value "Win11Ready" with blocable reason or Ready
     .AUTHOR
     Florian VALENTE
     .NOTES
@@ -19,7 +18,6 @@ $ErrorActionPreference = "SilentlyContinue"
 # Registry key to populate
 $RegPath = "HKLM:\SOFTWARE\BlackCat"
 $RegName = "Win11Ready"
-$RegName1 = "Win11Reason"
 
 # Custom definition for checking CPU family
 $CPUDefinition = @"
@@ -208,7 +206,7 @@ $Win11Report
 $BuildNumber = (Get-WmiObject Win32_OperatingSystem).BuildNumber
 If ($BuildNumber -ge 22000) {
     Write-Host "This device is already on Windows 11" -ForegroundColor Green
-    Set-ItemProperty $RegPath -Name $RegName -Value $true -Force | Out-Null
+    Set-ItemProperty $RegPath -Name $RegName -Value "Ready" -Force | Out-Null
     exit 0
 }
 $Processor = @(Get-WmiObject Win32_Processor)[0]
@@ -273,11 +271,9 @@ $Results
 # Display final result
 if ($Results.psobject.properties.value -contains $false) {
     Write-Host "This device is not compatible with Windows 11" -ForegroundColor Red
-    Set-ItemProperty $RegPath -Name $RegName -Value $false -Force | Out-Null
-    Set-ItemProperty $RegPath -Name $RegName1 -Value (($Results.psobject.Properties | ? {$_.value -eq $false}).Name -join ", ") -Force | Out-Null
+    Set-ItemProperty $RegPath -Name $RegName -Value (($Results.psobject.Properties | ? {$_.value -eq $false}).Name -join ", ") -Force | Out-Null
 }
 else {
     Write-Host "This device is compatible with Windows 11" -ForegroundColor Green
-    Set-ItemProperty $RegPath -Name $RegName -Value $true -Force | Out-Null
-    Set-ItemProperty $RegPath -Name $RegName1 -Value "" -Force | Out-Null
+    Set-ItemProperty $RegPath -Name $RegName -Value "Ready" -Force | Out-Null
 }
